dd486988d20c7fca2a890f5dc1e5c2bc85944dc1
Adrian Szczechura <a.szczechura.98@gmail.com>
2021-12-30 02:33:26 +0100

    feat: add SignUp/SignIn screens

--- src/navigation/no-auth/index.tsx
+++ src/navigation/no-auth/index.tsx
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationRoutes} from '../../navigation/routes';
 
 import {FirstScreen} from '../../screens/no-auth-screens/first-screen';
+import {SignInScreen} from '../../screens/no-auth-screens/sign-in';
 
 const NoAuthStack = createNativeStackNavigator<any>();
 
@@ -13,6 +14,20 @@ export const NoAuthNavigation = (): ReactElement => {
       <NoAuthStack.Screen
         name={NavigationRoutes.FIRST_SCREEN}
         component={FirstScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <NoAuthStack.Screen
+        name={NavigationRoutes.SIGN_IN}
+        component={SignInScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <NoAuthStack.Screen
+        name={NavigationRoutes.SIGN_UP}
+        component={SignInScreen}
         options={{
           headerShown: false,
         }}
--- src/screens/no-auth-screens/first-screen/index.tsx
+++ src/screens/no-auth-screens/first-screen/index.tsx
@@ -1,10 +1,34 @@
+import {useNavigation} from '@react-navigation/native';
 import React, {ReactElement} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {Button, SafeAreaView, Text} from 'react-native';
+import {NavigationRoutes} from '../../../navigation/routes';
+import {StackNavigationProp} from '@react-navigation/stack';
 
 export const FirstScreen = (): ReactElement => {
+  const navigation = useNavigation<StackNavigationProp<any>>();
   return (
     <SafeAreaView>
       <Text>First Screen</Text>
+      <Button
+        title="Sign In"
+        onPress={() =>
+          navigation.navigate({
+            name: NavigationRoutes.SIGN_IN,
+            key: NavigationRoutes.SIGN_IN,
+          })
+        }>
+        Sign In
+      </Button>
+      <Button
+        title="Sign Up"
+        onPress={() =>
+          navigation.navigate({
+            name: NavigationRoutes.SIGN_UP,
+            key: NavigationRoutes.SIGN_UP,
+          })
+        }>
+        Sign In
+      </Button>
     </SafeAreaView>
   );
 };
