e9f413b95df3990072394285f4863a3003948256
Adrian Szczechura <a.szczechura.98@gmail.com>
2022-01-01 12:10:49 +0100

    feat: add ts path import handling

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,30 @@
  * @format
  */
 
+const path = require('path');
+
 module.exports = {
+  resolver: {
+    extraNodeModules: {
+      api: path.resolve(__dirname, 'src/api'),
+      assets: path.resolve(__dirname, 'src/assets'),
+      components: path.resolve(__dirname, 'src/components'),
+      constants: path.resolve(__dirname, 'src/constants'),
+      containers: path.resolve(__dirname, 'src/containers'),
+      config: path.resolve(__dirname, 'src/config'),
+      helpers: path.resolve(__dirname, 'src/helpers'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      locales: path.resolve(__dirname, 'src/locales'),
+      modules: path.resolve(__dirname, 'src/modules'),
+      navigation: path.resolve(__dirname, 'src/navigation'),
+      providers: path.resolve(__dirname, 'src/providers'),
+      screens: path.resolve(__dirname, 'src/screens'),
+      styles: path.resolve(__dirname, 'src/styles'),
+      types: path.resolve(__dirname, 'src/types'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      globals: path.resolve(__dirname, 'src/globals'),
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
